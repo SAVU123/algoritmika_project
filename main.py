658f6c5012a3a4c4b72a486624010012c16b40fa
SAVU123 <116507502+SAVU123@users.noreply.github.com>
2022-12-04 18:03:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello World')
+class TestWin(QWidget):
+    def __init__(self):
+    super().__init__()
+    self.set_appear()
+    self.initUI()
+    self.connects()
+    self.show()
+
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+        txt_title = 'Здоровье'
+        win_x, win_y = 200, 100
+        win_width, win_height = 1000, 600
+
